9ebf51351e027835dd938607254ef13aaa205f81
Smile <1072652010@qq.com>
2020-10-18 15:39:30 +0800

    更改swagger2，对接前端的@RequestBody

--- CitigroupCup/src/main/java/com/citi/group/poweru/PoweruApplication.java
+++ CitigroupCup/src/main/java/com/citi/group/poweru/PoweruApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableTransactionManagement
+@EnableSwagger2
 @MapperScan("com.citi.group.poweru.module.dao")
 public class PoweruApplication {
 
